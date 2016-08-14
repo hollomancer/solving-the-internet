328137817d9c7dd9f9085878d1970997a2b2c9f2
conrad hollomon <chollomon@gmail.com>
2016-08-14 03:26:51 +0000

    Add install.sh to prep docker

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) apparmor
+sudo apt-get update
+sudo apt-get install docker-engine
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
